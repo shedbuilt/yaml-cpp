a3ea6295e009ef15d760ad0b48b08c70b1017a23
Auston Stewart <auston@shedbuilt.net>
2018-02-12 19:31:05 -1000

    Attempting to install yaml-cpp in a more appropriate location

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -pv build
 cd build
-cmake -DBUILD_SHARED_LIBS=ON .. && \
+cmake -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_INSTALL_PREFIX=/usr \
+      .. && \
 make -j $SHED_NUMJOBS && \
 make DESTDIR="$SHED_FAKEROOT" install
